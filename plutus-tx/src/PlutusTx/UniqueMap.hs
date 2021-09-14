71542f360b8029557e786092f41d305c4cff59c8
Anton Myasnikov <awkure@protonmail.ch>
2021-09-14 18:31:03 +0300

    Add UniqueMap type to plutus-tx

---
+++ plutus-tx/src/PlutusTx/UniqueMap.hs
@@ -0,0 +1,46 @@
+{- | An association data structure, suitable for on-chain use, which preserves
+ the uniqueness of its keys.
+
+ This module is designed for importing qualified. The interface is based on
+ 'PlutusTx.AssocMap', but with less boolean blindness.
+
+ = Note
+
+ All time complexities assume \(\Theta(1)\) order comparisons for keys.
+-}
+module PlutusTx.UniqueMap (
+  -- * Types
+  Internal.UniqueMap,
+  Internal.Inclusion (..),
+
+  -- * Construction
+  Internal.empty,
+  Internal.singleton,
+  Internal.fromList,
+
+  -- * Queries
+  Internal.size,
+  Internal.lookup,
+  Internal.findMin,
+  Internal.findMax,
+  Internal.inclusion,
+
+  -- * Updates
+  Internal.insert,
+  Internal.delete,
+  Internal.alter,
+  Internal.mapWithKey,
+  Internal.mapMaybe,
+  Internal.mapMaybeWithKey,
+
+  -- * Combination
+  Internal.union,
+  Internal.unionWith,
+
+  -- * Conversion
+  Internal.toList,
+  Internal.keys,
+  Internal.elems,
+) where
+
+import qualified PlutusTx.UniqueMap.Internal as Internal
